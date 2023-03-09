c5046148f0bd2af99501a4fe04dc3034edcd317b
EvanFarah <96839483+EvanFarah@users.noreply.github.com>
2023-03-08 19:48:26 -0500

    Update AI-Coder-Bot.py

--- AI-Coder-Bot.py
+++ AI-Coder-Bot.py
@@ -2,4 +2,9 @@ import requests
  
 # Making a get request
 response = requests.get('https://api.github.com/')
-If 
+from google import google
+num_page = 1
+search_results = google.search(response, num_page)
+for result in search_results:
+    print(result.description)
+ num_page2 = 2
